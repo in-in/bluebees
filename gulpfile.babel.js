3548ae11eb3a69424c14bb0dcb77c89ee520eed8
in-in <0866222@gmail.com>
2017-06-11 05:44:51 +0300

    add sass task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import del from 'del';
 import browserSync from 'browser-sync';
 import pug from 'gulp-pug';
+import sass from 'gulp-sass';
 import mjml from 'gulp-mjml';
 import notify from 'gulp-notify';
 import imagemin from 'gulp-imagemin';
@@ -13,6 +14,10 @@ const paths = {
   images: {
     get src () { return `${paths.src}assets/images/**/*.*`; },
     get dist () { return `${paths.dist}assets/images/`; }
+  },
+  sass: {
+    get src () { return `${paths.src}sass/*.sass`; },
+    get dist () { return `${paths.src}css/`; }
   }
 };
 
@@ -34,13 +39,29 @@ const serve = (done) => {
   done();
 }
 
-const clean = () => del([paths.dist]);
+const clean = () => del([paths.dist, paths.src + 'css']);
 
 const images = () => {
   return gulp.src(paths.images.src)
     .pipe(imagemin())
     .pipe(gulp.dest(paths.images.dist))
 };
+
+const css = () => {
+  return gulp.src(paths.sass.src)
+    .pipe(plumber({
+      errorHandler: notify.onError(function(err) {
+        return {
+           title: err.plugin.toUpperCase(),
+           message: err.message
+        }
+      })
+    }))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest(paths.sass.dist))
+}
 
 const build = () => {
   return gulp.src(paths.src + '*.pug')
@@ -59,11 +80,13 @@ const build = () => {
 };
 
 const watch = () => {
-  gulp.watch(paths.src, gulp.series(build, reload));
+  gulp.watch(paths.src, {
+    ignored: paths.src + 'css/'
+  }, gulp.series(css, build, reload));
   gulp.watch(paths.images.src, gulp.series(images, reload));
 }
 
-export const dist = gulp.series(clean, build, images);
-const dev = gulp.series(clean, build, images, serve, watch);
+export const dist = gulp.series(clean, css, build, images);
+const dev = gulp.series(clean, css, build, images, serve, watch);
 
 export default dev;
